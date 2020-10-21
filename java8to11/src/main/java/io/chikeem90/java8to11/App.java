0fb22d3abbed564ad9014c926463ae996804c649
chikeem90 <chikeem90@gmail.com>
2020-10-21 21:06:00 +0900

    Java 8 API 기본 메소드와 스태틱 메소드

--- java8to11/src/main/java/io/chikeem90/java8to11/App.java
+++ java8to11/src/main/java/io/chikeem90/java8to11/App.java
@@ -1,17 +1,26 @@
 package io.chikeem90.java8to11;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
 public class App {
     public static void main(String[] args) {
-        Foo foo = new DefaultFoo("keesun");
-        foo.printName();
-        foo.printNameUpperCase();
-
-        Foo.printAnything();
+        List<String> name = new ArrayList<>();
+        name.add("keesun");
+        name.add("whiteship");
+        name.add("toby");
+        name.add("foo");
+//        name.forEach(System.out::println);
+//        Spliterator<String> spliterator = name.spliterator();
+//        Spliterator<String> spliterator1 = spliterator.trySplit();
+//        while (spliterator.tryAdvance(System.out::println));
+//        System.out.println("=======================");
+//        while (spliterator1.tryAdvance(System.out::println));
+//        name.removeIf(s -> s.startsWith("k"));
+        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+        name.sort(compareToIgnoreCase.reversed());
+        name.forEach(System.out::println);
     }
 }
